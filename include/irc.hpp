5fdcbe22eba88f3056bb87a116b86c16cd0d7af0
llalba <llalba@student.42.fr>
2022-10-19 15:59:58 +0200

    🎣 First headers

---
+++ include/irc.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   irc.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llalba <llalba@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 14:29:39 by llalba            #+#    #+#             */
+/*   Updated: 2022/10/19 14:31:40 by llalba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IRC_HPP
+# define IRC_HPP
+
+# define RED "\033[1;31m"
+# define GRN "\033[1;32m"
+# define YEL "\033[1;33m"
+# define BLU "\033[1;34m"
+# define MAG "\033[1;35m"
+# define CYN "\033[1;36m"
+# define WHT "\033[1;37m"
+# define END "\033[0m"
+# include <iostream>
+# include <string>
+
+#endif
