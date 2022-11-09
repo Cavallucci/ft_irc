b6a0cb58e256f79c0b89722282f5705bcf3eb2d1
Laura CAVALLUCCI <lcavallu@e1r9p10.clusters.42paris.fr>
2022-11-09 12:03:23 +0100

    print ip function p.28

--- src/server.cpp
+++ src/server.cpp
@@ -6,6 +6,7 @@ server::server() : _port(""), _host(""), _pwd("")
 server::server(std::string port, std::string pwd): _port(port), _pwd(pwd), _host("localhost")
 {
     _setUp();
+    _printIP(_host);
 }
 
 server::~server()
@@ -17,8 +18,7 @@ void    server::_setUp(void)
     struct addrinfo hints;
     struct addrinfo *servinfo; // will point to the results
 
-    memset(&hints, 0, sizeof hints); // make sure the struct is
-    empty
+    memset(&hints, 0, sizeof(hints)); // make sure the struct is empty
 
     hints.ai_family = AF_UNSPEC; // don't care IPv4 or IPv6
     hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
@@ -34,4 +34,55 @@ void    server::_setUp(void)
     addrinfos
     // ... do everything until you don't need servinfo anymore
     freeaddrinfo(servinfo); // free the linked-list
+}
+
+int    server::printIP(std::string host)
+{
+    struct addrinfo hints, *res, *p;
+	int status;
+	char ipstr[INET6_ADDRSTRLEN];
+
+	if (!host) 
+    {
+	    fprintf(stderr,"usage: showip hostname\n");
+	    return 1;
+	}
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ((status = getaddrinfo(host, NULL, &hints, &res)) != 0) 
+    {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+		return 2;
+	}
+
+	printf("IP addresses for %s:\n\n", host);
+
+	for(p = res;p != NULL; p = p->ai_next) 
+    {
+		void *addr;
+		char *ipver;
+
+		// get the pointer to the address itself,
+		// different fields in IPv4 and IPv6:
+		if (p->ai_family == AF_INET) { // IPv4
+			struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+			addr = &(ipv4->sin_addr);
+			ipver = "IPv4";
+		} else { // IPv6
+			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+			addr = &(ipv6->sin6_addr);
+			ipver = "IPv6";
+		}
+
+		// convert the IP to a string and print it:
+		inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+		printf("  %s: %s\n", ipver, ipstr);
+	}
+
+	freeaddrinfo(res); // free the linked list
+
+	return 0;
 }
